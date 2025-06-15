7cf0789e6779bbc9a2d71b514aac378147bb3df1
David Chen <chen7daivd@me.com>
2025-06-15 07:48:36 -0400

    feat: setup validation shared dtos and test on web

---
+++ packages/entix-sdk/src/dtos/user.dto.ts
@@ -0,0 +1,4 @@
+import { z } from 'zod';
+import { createUserSchema } from '../schemas/user.schema';
+
+export type CreateUserDto = z.infer<typeof createUserSchema>;
---
+++ packages/entix-sdk/src/index.ts
@@ -0,0 +1,2 @@
+export * from './schemas/user.schema';
+export * from './dtos/user.dto';
