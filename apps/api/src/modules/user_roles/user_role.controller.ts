cc3fa8a9fbd2d9c9a151f90d3b54c46c73a564ba
David Chen <chen7daivd@me.com>
2025-06-19 12:34:38 -0400

    feat: removed suport for role centric relation

--- apps/api/src/modules/user_roles/user_role.controller.ts
+++ apps/api/src/modules/user_roles/user_role.controller.ts
@@ -8,12 +8,13 @@ import {
 } from '@repo/entix-sdk';
 import { Delete, JsonController, Params, Post, UseBefore } from 'routing-controllers';
 import { UserRoleService } from './user_role.service';
+import { Injectable } from '@utils/typedi.util';
 
+@Injectable()
 @JsonController('/v1/user-roles')
 export class UserRoleController {
   constructor(private readonly userRoleService: UserRoleService) {}
 
-  @Post('/roles/:roleId/users/:userId')
   @Post('/users/:userId/roles/:roleId')
   @UseBefore(validateParams(createUserRoleSchema))
   async createUserRole(@Params() params: CreateUserRoleDto): Promise<SuccessResultDto> {
@@ -21,7 +22,6 @@ export class UserRoleController {
     return { success: true };
   }
 
-  @Delete('/roles/:roleId/users/:userId')
   @Delete('/users/:userId/roles/:roleId')
   @UseBefore(validateParams(deleteUserRoleSchema))
   async deleteUserRole(@Params() params: DeleteUserRoleDto): Promise<SuccessResultDto> {
