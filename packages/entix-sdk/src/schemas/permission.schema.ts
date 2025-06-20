18453d1cf8e0cfc4f86397c3ecb2ec694ac4667f
David Chen <chen7daivd@me.com>
2025-06-20 17:29:53 -0400

    chore: add string to integer conversion in zod validaiton for permissionCode

--- packages/entix-sdk/src/schemas/permission.schema.ts
+++ packages/entix-sdk/src/schemas/permission.schema.ts
@@ -2,12 +2,12 @@ import { z } from 'zod';
 
 export const createPermissionSchema = z.object({
   name: z.string().min(3),
-  permissionCode: z.number().int().positive(),
+  permissionCode: z.coerce.number().int().positive(),
   description: z.string().optional(),
 });
 
 export const updatePermissionSchema = z.object({
   name: z.string().min(3).optional(),
-  permissionCode: z.number().int().positive().optional(),
+  permissionCode: z.coerce.number().int().positive().optional(),
   description: z.string().optional().nullable(),
 });
