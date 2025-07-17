862479679399852917d787c4e5912b2bf7161bc3
David Chen <chen7daivd@me.com>
2025-07-17 19:28:54 -0400

    chore: add stating to api

--- apps/api/src/constants/app.constant.ts
+++ apps/api/src/constants/app.constant.ts
@@ -1,11 +1,13 @@
 export enum AppEnv {
   Development = 'development',
+  Staging = 'staging',
   Production = 'production',
   Test = 'test',
 }
 
 export const AppEnvFileName: Record<AppEnv, string> = {
   [AppEnv.Development]: '.env',
+  [AppEnv.Staging]: '.env',
   [AppEnv.Test]: 'test.env',
   [AppEnv.Production]: '.env',
 };
