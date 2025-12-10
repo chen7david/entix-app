54c8e4e5ba3c2c511cd1b68e38f32c0f95dec552
David Chen <chen7david@me.com>
2025-12-10 12:58:15 -0500

    chore: setup static local dev ports

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,4 +5,13 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 8000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 })
