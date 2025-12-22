1a0b145901ba83d01b542ccc1ebfcb1cb57fa12c
David Chen <chen7david@me.com>
2025-12-22 11:15:48 -0500

    chore: added example migrations

--- api/db/schema.db.ts
+++ api/db/schema.db.ts
@@ -1,4 +1,6 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "@hono/zod-openapi";
 
 export const usersTable = sqliteTable("users", {
     id: text("id")
@@ -8,4 +10,23 @@ export const usersTable = sqliteTable("users", {
     username: text("username").notNull().unique(),
     email: text("email").notNull().unique(),
     password: text("password").notNull(),
+    sex: integer("sex").notNull().default(1),
 });
+
+export const userInsertSchema = createInsertSchema(usersTable);
+
+export const userCreateSchema = createInsertSchema(usersTable, {
+    username: (s) => z.string().min(3).max(30).regex(/^[a-zA-Z0-9_]+$/, "Only letters, numbers, underscore"),
+    email: (s) => z.email(),
+    password: () => z.string().min(8).max(200),
+    sex: () => z.number().default(1),
+}).omit({
+    id: true,
+});
+
+export const userSelectSchema = createSelectSchema(usersTable).omit({
+    password: true,
+});
+
+export type UserPublic = z.infer<typeof userSelectSchema>;
+export type UserCreate = z.infer<typeof userCreateSchema>;
