0e598acfae7909db34bf185b5fd5414b7e5413cd
David Chen <chen7david@me.com>
2025-12-21 21:05:56 -0500

    chore: user schema changed name to username

--- api/db/schema.db.ts
+++ api/db/schema.db.ts
@@ -2,7 +2,7 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
     id: int("id").primaryKey({ autoIncrement: true }),
-    name: text("name").notNull(),
+    username: text("username").notNull().unique(),
     email: text("email").notNull().unique(),
     password: text("password").notNull(),
 });
