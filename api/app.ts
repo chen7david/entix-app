32835ad6088c122f5a191fce6681730fa058f69c
David Chen <chen7david@me.com>
2025-12-23 21:04:08 -0500

    chore: setup better auth

--- api/app.ts
+++ api/app.ts
@@ -2,12 +2,12 @@ import { createApp } from "./lib/app.lib";
 import { configureOpenApi } from "./lib/open-api.lib";
 import { mountRoutes } from "./lib/app.lib";
 import { routes } from "./routes/index.route";
+import { mountBetterAuth } from "./lib/better-auth";
 
 const app = createApp();
 
 configureOpenApi(app);
+mountBetterAuth(app);
 mountRoutes({ app, routes, prefix: '/api/v1' });
-
-
 
 export default app;
---
+++ api/lib/better-auth/options.ts
@@ -0,0 +1,6 @@
+import { BetterAuthOptions } from 'better-auth';
+
+export const betterAuthOptions: BetterAuthOptions = {
+    appName: 'entix-app',
+    basePath: '/api/v1/auth',
+};
