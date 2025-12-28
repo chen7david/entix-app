78ab5005e013ca31aeba92c4d20298e17ac00773
David Chen <chen7david@me.com>
2025-12-27 21:36:06 -0500

    chore: snapshot

--- api/lib/better-auth/index.ts
+++ api/lib/better-auth/index.ts
@@ -1,25 +1,47 @@
 import { betterAuth } from "better-auth";
-import { AppOpenApi } from "@api/helpers/types.helpers";
+import { AppContext, AppOpenApi } from "@api/helpers/types.helpers";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/d1";
 import { betterAuthOptions } from "./options";
 import * as schema from "./../../db/schema.db";
+import { Mailer } from "./../mail/mailer.lib";
 
-export const auth = (env: CloudflareBindings) => {
-    const db = drizzle(env.DB, { schema });
+export const auth = (ctx: AppContext) => {
+    const db = drizzle(ctx.env.DB, { schema });
+    const mailer = new Mailer(ctx);
 
     return betterAuth({
+        database: drizzleAdapter(db, { provider: "sqlite" }),
         ...betterAuthOptions,
-        baseURL: env.BETTER_AUTH_URL,
-        secret: env.BETTER_AUTH_SECRET,
+        baseURL: ctx.env.BETTER_AUTH_URL,
+        secret: ctx.env.BETTER_AUTH_SECRET,
         emailAndPassword: {
             enabled: true,
+            requireEmailVerification: true,
         },
-        database: drizzleAdapter(db, { provider: "sqlite" }),
+        emailVerification: {
+            sendOnSignUp: true,
+            sendVerificationEmail: async ({ user, url }) => {
+                void mailer.sendTemplate({
+                    to: user.email,
+                    templateId: "email-verification",
+                    variables: {
+                        DISPLAY_NAME: user.name,
+                        VERIFICATION_LINK: url,
+                    },
+                });
+            }
+        },
+        security: {
+            allowOrigin: ['http://localhost:3000', 'https://entix.app']
+        },
+        advanced: {
+            disableCSRFCheck: true
+        }
     });
 };
 
 export const mountBetterAuth = (app: AppOpenApi) => {
-    app.on(["GET", "POST"], "/api/v1/auth/*", (c) => auth(c.env).handler(c.req.raw));
+    app.on(["GET", "POST"], "/api/v1/auth/*", (c) => auth(c).handler(c.req.raw));
 
 }
