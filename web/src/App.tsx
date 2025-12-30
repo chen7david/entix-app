2446b1b28dc4963ecbf7ce4b23322377af13db0d
David Chen <chen7david@me.com>
2025-12-30 10:22:07 -0500

    chore: setup basic example layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/home/HomePage";
 import { SignUpPage } from "./pages/signup/SignUpPage";
+import { AuthLayout } from "./layouts/AuthLayout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route index element={<HomePage />} />
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="signup" element={<SignUpPage />} />
+      </Route>
     </Routes>
   );
 }
