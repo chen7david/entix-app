24b00a2aa1401aed04b3377c38d05c36acea70ff
David Chen <chen7david@me.com>
2026-01-18 09:52:43 -0500

    chore: cleaned up code

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { ThemeProvider } from './providers/ThemeProvider.tsx'
 import './index.css'
 import App from './App.tsx'
 
-import { ThemeProvider } from './providers/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
