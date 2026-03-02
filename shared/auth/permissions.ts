21f56153f72189c4624a31ec49fa074f8e5dbd06
chen7david <chen7david@me.com>
2026-03-02 10:03:29 -0500

    chore: remove unused code

--- shared/auth/permissions.ts
+++ shared/auth/permissions.ts
@@ -1,7 +1,7 @@
 import { createAccessControl } from "better-auth/plugins/access";
 
 export const statement = {
-    project: ["create", "share", "update", "delete"],
+    organization: ["update", "delete"],
     invitation: ["create", "cancel"],
     member: ["read", "create", "update", "delete"],
 } as const;
@@ -10,16 +10,15 @@ export const ac = createAccessControl(statement);
 
 export const roles = {
     member: ac.newRole({
-        project: ["create"],
         member: ["read"],
     }),
     admin: ac.newRole({
-        project: ["create", "update"],
+        organization: ["update"],
         invitation: ["create", "cancel"],
         member: ["read", "create", "update", "delete"],
     }),
     owner: ac.newRole({
-        project: ["create", "update", "delete"],
+        organization: ["update", "delete"],
         invitation: ["create", "cancel"],
         member: ["read", "create", "update", "delete"],
     }),
