9fa7c0698e29a81bca4fdb344933bd7701ea34f4
chen7david <chen7david@me.com>
2026-03-28 11:37:38 -0400

    test: 001 - n8n codereview

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,7 +8,6 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./providers/ThemeProvider.tsx";
 import { getRequiredElementById } from "./utils/dom.ts";
 
-// Satisfy Biome's zero-! policy by using a safe DOM helper that throws if root is missing.
 const rootElement = getRequiredElementById("root");
 
 createRoot(rootElement).render(
